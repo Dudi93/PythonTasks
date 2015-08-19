d1490c6c6bf91d851d9f9b9201d64b3b216d2154
Dudi93 <kamilek93@gmail.com>
2015-08-19 16:54:31 +0200

    shitty shit as shitty fuck

--- CurrentSet/src/currentset.py
+++ CurrentSet/src/currentset.py
@@ -12,40 +12,32 @@ def parse(fobj):
     '''
 
     root = ES.parse(fobj)
-    #Data ={"Target name":"", "Target IP":"", "HW type":""}
-    Targets=[]
-    IPs=[]
-    HW=[]
-    allData=[]
-    test2 = []
+    data = []
     tbllst = root.findall('.//table')
     for elem in tbllst[0][0]:  # iterating over table/tbody/tr elements
         if elem.tag == 'tr':
             counter = 1
-            test=[]
+            temp=[]
             for td in elem:
                 if counter == 1:
-                    Targets.append(td.text)
-                    test.append(td.text)
+                    temp.append(td.text)
                     counter += 1
                 elif counter == 3:
-                    IPs.append(td.text)
-                    test.append(td.text)
+                    temp.append(td.text)
                     counter += 1
                 elif counter == 10:
-                    HW.append(td.text)
                     if td.text != None:
-                        test.append(td.text)
+                        temp.append(td.text)
                     counter += 1
                 else:
                     counter += 1
-            if len(test) == 3:
-                test2.append(test)
+            if len(temp) == 3:
+                data.append(temp)
                 
-    test2.sort(key=lambda item: (item[2], item[0]), reverse=False)
+    data.sort(key=lambda item: (item[2], item[0]), reverse=False)
     #allData = sorted(allData, key = lambda item: (item[0], item[1]))
                 
-    return test2
+    return data
 
 
 def report(targets):
@@ -69,7 +61,7 @@ object
     '''
     
     #dwa fory tu dopierdolic i chuj bedzie kurwa
-    print targets[0][1]
+    print targets
     
     #sorted1=sorted(targets, key=lambda item: item(9))
     #sorted2=sorted(targets, key=lambda item: item(0))
