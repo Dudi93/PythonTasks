51e5f9896ce9e6fd55fe960df407a411acb30764
Dudi93 <kamilek93@gmail.com>
2015-08-19 16:49:02 +0200

    shitty shit as shitty fuck

--- CurrentSet/src/currentset.py
+++ CurrentSet/src/currentset.py
@@ -12,30 +12,40 @@ def parse(fobj):
     '''
 
     root = ES.parse(fobj)
+    #Data ={"Target name":"", "Target IP":"", "HW type":""}
+    Targets=[]
+    IPs=[]
+    HW=[]
+    allData=[]
+    test2 = []
     tbllst = root.findall('.//table')
     for elem in tbllst[0][0]:  # iterating over table/tbody/tr elements
         if elem.tag == 'tr':
-            field = 1
+            counter = 1
+            test=[]
             for td in elem:
-                if field == 1:
-                    if td.text == "Target name":
-                        field = field + 1
-                        continue
-                    print td.text
-                    field = field + 1
-                elif field == 3:
-                    if td.text == "Target IP":
-                        field = field + 1
-                        continue
-                    print td.text
-                    field = field + 1
-                elif field == 10:
-                    if td.text == "HW type":
-                        field = field + 1
-                        continue
-                    print td.text
-                    field = field + 1
-                field = field + 1
+                if counter == 1:
+                    Targets.append(td.text)
+                    test.append(td.text)
+                    counter += 1
+                elif counter == 3:
+                    IPs.append(td.text)
+                    test.append(td.text)
+                    counter += 1
+                elif counter == 10:
+                    HW.append(td.text)
+                    if td.text != None:
+                        test.append(td.text)
+                    counter += 1
+                else:
+                    counter += 1
+            if len(test) == 3:
+                test2.append(test)
+                
+    
+    allData = sorted(allData, key = lambda item: (item[0], item[1]))
+                
+    print test2
 
 
 def report(targets):
@@ -49,16 +59,22 @@ def report(targets):
     <Target name> <Target IP>
 
     <HW type>
-
+object
     <Target name> <Target IP>
     <Target name> <Target IP>
     ...
     <Target name> <Target IP>
-    ...
+    ...return 
 
     '''
-
-    # !!!Your code here!!!
+    print targets
+    
+    #sorted1=sorted(targets, key=lambda item: item(9))
+    #sorted2=sorted(targets, key=lambda item: item(0))
+    
+    #for elem in sorted1:
+        #if elem
+    #    print elem
 
 
 def main():
